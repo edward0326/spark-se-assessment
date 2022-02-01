73492259cdc26e95643f20d18c15436827e0a52d
edward0326 <57732219+edward0326@users.noreply.github.com>
2022-01-31 22:26:47 -0500

    Added /users/index functionality

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -20,7 +20,7 @@ class RegisterAPI(MethodView):
 
     def post(self):
         # get the post data
-        post_data = request.get_json(); print(request)
+        post_data = request.get_json()
         # check if user already exists
         user = User.query.filter_by(email=post_data.get('email')).first()
         if not user:
@@ -38,7 +38,7 @@ class RegisterAPI(MethodView):
                 responseObject = {
                     'status': 'success',
                     'message': 'Successfully registered.',
-                    'auth_token': auth_token.decode()
+                    'auth_token': auth_token
                 }
                 return make_response(jsonify(responseObject)), 201
             except Exception as e:
@@ -54,13 +54,26 @@ class RegisterAPI(MethodView):
             }
             return make_response(jsonify(responseObject)), 202
 
+class GetUsersAPI(MethodView):
+    def get(self):
+        returnArr = ''
+        users = User.query.all()
+        for u in users:
+            returnArr += '{Admin: ' + str(u.admin) + ', Email: ' + u.email + ', ID: ' + str(u.id) + ', Registered_on: ' + str(u.registered_on) + '} \n'
+        return returnArr
 
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
+getusers_view = GetUsersAPI.as_view('getuser_api')
 
 # add Rules for API Endpoints
 auth_blueprint.add_url_rule(
     '/auth/register',
     view_func=registration_view,
     methods=['POST', 'GET']
+)
+auth_blueprint.add_url_rule(
+    '/users/index',
+    view_func=getusers_view,
+    methods=['GET']
 )
