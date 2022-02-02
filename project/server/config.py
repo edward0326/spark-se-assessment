a5c2b1449d5cda7bc1c5a79c61a458acb3be1383
edward0326 <57732219+edward0326@users.noreply.github.com>
2022-02-01 20:02:27 -0500

    config.py

--- project/server/config.py
+++ project/server/config.py
@@ -16,6 +16,7 @@ class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
+    DATABASE_URL='sqlite:///'
     SQLALCHEMY_DATABASE_URI = 'postgres://jwtfyixxcrlluu:8dccebdefa2cce362064957062d223e8cef9c0b20c7fe6be06b7e59c7b002924@ec2-54-173-31-84.compute-1.amazonaws.com:5432/d2ip57ffqm183b'
 
 
