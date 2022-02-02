7680a2873652f708b9860e746aed1779796889e0
edward0326 <57732219+edward0326@users.noreply.github.com>
2022-02-01 19:36:38 -0500

    config.py

--- project/server/config.py
+++ project/server/config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgres://jwtfyixxcrlluu'
-database_name = 'DATABASE_URL'
+postgres_local_base = 'postgres://'
+database_name = 'diagnostic'
 
 
 class BaseConfig:
